82a982076d3b3378e7a219ca836b46c4dd222f0b
Lasha Efremidze <efremidzel@hotmail.com>
2025-04-12 18:58:58 -0700

    updated models

--- InfiniteView/Model/Item.swift
+++ InfiniteView/Model/Item.swift
@@ -7,25 +7,10 @@
 
 import SwiftUI
 
-/// Sample Data Model
-struct Item: Identifiable {
-    var id: UUID = .init()
-    var color: Color
-}
-
 struct RandomCircleGroupView: View {
-    let circles = [
-        CircleData(image: "m4", size: 200, offsetx: 0, offsety: 50, color: .red),
-        CircleData(image: "m5", size: 100, offsetx: 50, offsety: -100, color: .orange),
-        CircleData(image: "m2", size: 150, offsetx: -90, offsety: -120, color: .green),
-        CircleData(image: "m3", size: 150, offsetx: -180, offsety: 10, color: .indigo),
-        CircleData(image: "m1", size: 150, offsetx: 130, offsety: -215, color: .purple),
-        CircleData(image: "m6", size: 180, offsetx: 190, offsety: -40, color: .cyan),
-        CircleData(image: "m7", size: 150, offsetx: 180, offsety: 140, color: .yellow)
-    ]
     var body: some View {
         ZStack {
-            ForEach(circles, id: \.id) { circle in
+            ForEach(CircleData.randomCircles, id: \.id) { circle in
                 Circle()
                     .fill(circle.color.opacity(0.5))
                     .frame(width: circle.size, height: circle.size)
@@ -48,4 +33,16 @@ struct CircleData: Identifiable {
     let offsetx: CGFloat
     let offsety: CGFloat
     let color: Color
+}
+
+extension CircleData {
+    static let randomCircles = [
+        CircleData(image: "m4", size: 200, offsetx: 0, offsety: 50, color: .red),
+        CircleData(image: "m5", size: 100, offsetx: 50, offsety: -100, color: .orange),
+        CircleData(image: "m2", size: 150, offsetx: -90, offsety: -120, color: .green),
+        CircleData(image: "m3", size: 150, offsetx: -180, offsety: 10, color: .indigo),
+        CircleData(image: "m1", size: 150, offsetx: 130, offsety: -215, color: .purple),
+        CircleData(image: "m6", size: 180, offsetx: 190, offsety: -40, color: .cyan),
+        CircleData(image: "m7", size: 150, offsetx: 180, offsety: 140, color: .yellow)
+    ]
 }
--- InfiniteView/View/Home.swift
+++ InfiniteView/View/Home.swift
@@ -9,15 +9,7 @@ import SwiftUI
 
 struct Home: View {
     /// Sample Items
-    @State private var items: [CircleData] = [
-        CircleData(image: "m4", size: 200, offsetx: 0, offsety: 50, color: .red),
-        CircleData(image: "m5", size: 100, offsetx: 50, offsety: -100, color: .orange),
-        CircleData(image: "m2", size: 150, offsetx: -90, offsety: -120, color: .green),
-        CircleData(image: "m3", size: 150, offsetx: -180, offsety: 10, color: .indigo),
-        CircleData(image: "m1", size: 150, offsetx: 130, offsety: -215, color: .purple),
-        CircleData(image: "m6", size: 180, offsetx: 190, offsety: -40, color: .cyan),
-        CircleData(image: "m7", size: 150, offsetx: 180, offsety: 140, color: .yellow)
-    ]
+    @State private var items: [CircleData] = CircleData.randomCircles
     
     var body: some View {
         ScrollView(.vertical) {
